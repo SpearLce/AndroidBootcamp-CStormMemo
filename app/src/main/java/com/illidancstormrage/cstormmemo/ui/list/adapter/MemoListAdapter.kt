ab8ee392d54bfb68f40fc59c09f380a4c35cd6a3
illidanCStormrage <2636538012@qq.com>
2024-05-05 11:18:37 +0800

    memo 添加memo删除功能，添加欢迎格，修改配色

--- app/src/main/java/com/illidancstormrage/cstormmemo/ui/list/adapter/MemoListAdapter.kt
+++ app/src/main/java/com/illidancstormrage/cstormmemo/ui/list/adapter/MemoListAdapter.kt
@@ -1,6 +1,7 @@
 package com.illidancstormrage.cstormmemo.ui.list.adapter
 
 
+import android.app.AlertDialog
 import android.view.LayoutInflater
 import android.view.ViewGroup
 import androidx.navigation.findNavController
@@ -10,6 +11,7 @@ import com.illidancstormrage.cstormmemo.model.memo.MemoRecord
 import com.illidancstormrage.cstormmemo.repository.LocalRepository
 import com.illidancstormrage.cstormmemo.ui.list.ListFragment
 import com.illidancstormrage.cstormmemo.ui.list.ListFragmentDirections
+import com.illidancstormrage.cstormmemo.ui.list.ListViewModel
 import com.illidancstormrage.cstormmemo.utils.extensions.msFormatDateStr
 import com.illidancstormrage.utils.toast.makeToast
 import kotlinx.coroutines.CoroutineScope
@@ -20,7 +22,8 @@ class MemoListAdapter(
     //提供环境
     private val fragment: ListFragment,
     //提供列表数据源
-    private val memoRecordList: List<MemoRecord>
+    private val memoRecordList: List<MemoRecord>,
+    private val listViewModel: ListViewModel
 ) : RecyclerView.Adapter<MemoListAdapter.ViewHolder>() {
 
 
@@ -31,7 +34,8 @@ class MemoListAdapter(
             binding.memoLastTime.text = record.lastEditTimeStamp.msFormatDateStr()
             CoroutineScope(Dispatchers.IO).launch {
                 if (record.categoryId != null) {
-                    val tagName = LocalRepository.categoryDao.selectById(record.categoryId!!).tagName
+                    val tagName =
+                        LocalRepository.categoryDao.selectById(record.categoryId!!).tagName
                     launch(Dispatchers.Main) {
                         binding.memoTag.text = tagName
                     }
@@ -41,6 +45,7 @@ class MemoListAdapter(
                     }
                 }
             }
+
         }
     }
 
@@ -49,14 +54,31 @@ class MemoListAdapter(
             ItemMemoListBinding.inflate(LayoutInflater.from(parent.context), parent, false)
         val holder = ViewHolder(binding)
 
+        //短按
         holder.itemView.setOnClickListener {
             val position = holder.bindingAdapterPosition
-            "[$position] -> ${memoRecordList[position].title}".makeToast()
+            //此处还是过时了，那么绑定在onBindViewHolder相关处
+            //"[$position] -> ${memoRecordList[position].title}".makeToast()
 
             //更新 editor_fragment_viewModel中
             //val editorViewModel =
-            val action = ListFragmentDirections.jumpToEdit(memoRecordList[position].id!!)
+            val action = ListFragmentDirections.jumpToEdit(memoRecordList[position].id)
             it.findNavController().navigate(action)
+        }
+        //长按
+        holder.itemView.setOnLongClickListener { view ->
+            val position = holder.bindingAdapterPosition
+            val alertDialog = AlertDialog.Builder(view.context)
+                .setTitle("注意")
+                .setMessage("是否确定删除该记录")
+                .setPositiveButton("确定") { dialog, _ ->
+                    listViewModel.deleteMemo(memoRecordList[position].id)
+                    dialog.dismiss()
+                }.setNegativeButton("取消") { dialog, _ ->
+                    dialog.dismiss()
+                }
+                .show()
+            true//消费长按事件，不会传递到其他长按绑定事件
         }
 
 
